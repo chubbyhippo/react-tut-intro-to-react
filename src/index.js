5a2acc08be618277356ad883ce52d1455d5798ff
ch <mkmsacc@outlook.com>
2020-12-27 18:40:19 +0700

    Add constructor to Board

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square"
-        onClick={() => this.setState({ value: "X" })}>
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
         {this.state.value}
       </button>
     );
@@ -20,6 +19,12 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
     return <Square value={i} />;
   }
@@ -66,5 +71,4 @@ class Game extends React.Component {
   }
 }
 
-ReactDOM.render(<Game />,
-  document.getElementById("root"));
+ReactDOM.render(<Game />, document.getElementById("root"));
