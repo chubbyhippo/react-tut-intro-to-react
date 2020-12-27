738a490e3c70caae418d6744e61efb830d5b16ec
ch <mkmsacc@outlook.com>
2020-12-27 20:18:18 +0700

    Add handleClick()

--- src/index.js
+++ src/index.js
@@ -25,6 +25,13 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
     return (
       <Square
